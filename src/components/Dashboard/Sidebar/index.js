92a996b383bec9dd16f732c03615f313bf60ff51
tmserl <tom.earl@hey.com>
2022-01-19 15:48:06 +0000

    Sidebar styling

--- src/components/Dashboard/Sidebar/index.js
+++ src/components/Dashboard/Sidebar/index.js
@@ -1,12 +1,20 @@
 import './Sidebar.css';
+import homeLogo from './home.svg';
+import resourcesLogo from './resources.svg';
 
 function SideBar() {
   return (
     <div className="sidebar">
-      <ul>
-        <li>Home</li>
-        <li>Resources</li>
-      </ul>
+      <div className="sidebar-nav">
+        <div className="sidebar-nav-btn sidebar-nav-btn--active">
+          <img src={homeLogo} className="home-logo" />
+          <p>Home</p>
+        </div>
+        <div className="sidebar-nav-btn">
+          <img src={resourcesLogo} className="resources-logo" />
+          <p>Resources</p>
+        </div>
+      </div>
       <h2>TEAM:</h2>
       <ul>
         <li>ashlie</li>
