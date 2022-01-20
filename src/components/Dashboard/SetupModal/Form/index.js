6ecd05ac4dec18a41bf2de842599475c940fd379
tmserl <tom.earl@hey.com>
2022-01-20 14:16:17 +0000

    Started form structure to collect responses into an object

---
+++ src/components/Dashboard/SetupModal/Form/index.js
@@ -0,0 +1,118 @@
+import { useState } from 'react';
+
+function Form() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Enter Team Name:
+        <input
+          type="text"
+          name="team_name"
+          value={inputs.team_name || ''}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Highlight Goal:
+        <input
+          type="text"
+          name="highlight_goal"
+          value={inputs.highlight_goal || ''}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Key Objective One:
+        <input
+          type="text"
+          name="key_objective_one"
+          value={inputs.key_objective_one || ''}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Key Objective Two:
+        <input
+          type="text"
+          name="key_objective_two"
+          value={inputs.key_objective_two || ''}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Key Objective Three:
+        <input
+          type="text"
+          name="key_objective_three"
+          value={inputs.key_objective_three || ''}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Time One:
+        <input
+          type="text"
+          name="time_one"
+          value={inputs.time_one || ''}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Time Two:
+        <input
+          type="text"
+          name="time_two"
+          value={inputs.time_two || ''}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Time Three:
+        <input
+          type="text"
+          name="time_three"
+          value={inputs.time_three || ''}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Time Four:
+        <input
+          type="text"
+          name="time_four"
+          value={inputs.time_four || ''}
+          onChange={handleChange}
+        />
+      </label>
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default Form;
+
+/* {
+    "team_name": "coding cats",
+    "highlight_goal": "team work makes the dram work!Lets get this API sorted",
+    "key_objective_one": "Try some GITHUB branching",
+    "key_objective_two": " function to call api",
+    "key_objective_three": "to make this happen",
+    "time_one": "Do the thing that does the thing",
+    "time_two": "By now we should be implementing a neat feature",
+    "time_three": "Deploy an awesome App!",
+    "time_four": "relax"
+    }
+*/
--- src/components/Dashboard/SetupModal/index.js
+++ src/components/Dashboard/SetupModal/index.js
@@ -1,13 +1,14 @@
 import './SetupModal.css';
+import Form from './Form';
 
-function SetupModal({ isVisible, makeModalHidden, makeModalVisible }) {
+function SetupModal({ isVisible, makeModalHidden }) {
   return (
-    <div id="myModal" className="modal " style={{ display: isVisible }}>
+    <div id="myModal" className="modal" style={{ display: isVisible }}>
       <div class="modal-content" style={{ display: isVisible }}>
         <span onClick={makeModalHidden} class="close">
           &times;
         </span>
-        <p>Some text in the Modal..</p>
+        <Form />
       </div>
     </div>
   );
