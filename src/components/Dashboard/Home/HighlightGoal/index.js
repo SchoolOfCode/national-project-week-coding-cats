54d30d6748022a2a325f95d1490885073c81b6f2
tmserl <tom.earl@hey.com>
2022-01-21 11:38:46 +0000

    Centered text in TeamName and HighlightGoal components

--- src/components/Dashboard/Home/HighlightGoal/index.js
+++ src/components/Dashboard/Home/HighlightGoal/index.js
@@ -2,7 +2,7 @@ import './HighlightGoal.css';
 
 function HighlightGoal({ highlightGoalText }) {
   return (
-    <div className="highlight card card-3">
+    <div className="highlight card card-3 card-center">
       <h2>Hackathon Highlight Goal</h2>
       <p>"{highlightGoalText}"</p>
     </div>
--- src/components/Dashboard/Home/TeamName/index.js
+++ src/components/Dashboard/Home/TeamName/index.js
@@ -2,7 +2,7 @@ import './TeamName.css';
 
 function TeamName({ teamNameText }) {
   return (
-    <div className="team-name card card-1">
+    <div className="team-name card card-1 card-center">
       <h2>{teamNameText}'s Dashboard</h2>
     </div>
   );
